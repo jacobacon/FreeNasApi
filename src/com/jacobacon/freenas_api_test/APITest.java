87b51fdf040ab43a1658845eb336ed0eabc69825
jacob <jacob@DESKTOP-FN7RGTB>
2016-07-20 23:46:52 -0500

    Initial commit. Doesn't do much yet.

---
+++ src/com/jacobacon/freenas_api_test/APITest.java
@@ -0,0 +1,54 @@
+package com.jacobacon.freenas_api_test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
+
+
+public class APITest {
+	public static void main(String[] args) {
+		try {
+			String webPage = "http://192.168.1.100/api/v1.0/system/version?format=json";
+			String name = "root";
+			String password = "Tiap123$";
+
+			String authString = name + ":" + password;
+			System.out.println("auth string: " + authString);
+			byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
+			String authStringEnc = new String(authEncBytes);
+			System.out.println("Base64 encoded auth string: " + authStringEnc);
+
+			URL url = new URL(webPage);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			
+			conn.setRequestProperty("Authorization", "Basic " + authStringEnc);
+			InputStream is = conn.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+
+			int numCharsRead;
+			char[] charArray = new char[1024];
+			StringBuffer sb = new StringBuffer();
+			while ((numCharsRead = isr.read(charArray)) > 0) {
+				sb.append(charArray, 0, numCharsRead);
+			}
+			String result = sb.toString();
+
+			System.out.println("*** BEGIN ***");
+			System.out.println(result);
+			System.out.println("*** END ***");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+}
